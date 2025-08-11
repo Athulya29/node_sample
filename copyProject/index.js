2ef2ef8e311b06ae13233d3db350e19dcfc075d5
Athulya A.R <athulyaarkrishna@gmail.com>
2025-08-11 22:32:29 +0530

    adding new file

---
+++ copyProject/index.js
@@ -0,0 +1,8 @@
+const sumOfDigits=require('sum-of-digits').default;
+
+let result=sumOfDigits(153);
+console.log(result);
+
+const sumOfTwoDigits=require('sum-of-two-numbers');
+let sum=sumOfTwoDigits(4,5);
+console.log(sum);
